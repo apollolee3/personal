812ab3d193ddfcf9ad998d599788ec1e4637d34b
apollolee3 <apollolee3@gmail.com>
2025-02-12 15:44:56 +0100

    Added favicon

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-300 to-white">
-      <h1 className="text-5xl font-bold text-gray-900">Apollo Lee</h1>
-      <p className="mt-4 text-lg text-gray-900">
-        I'm a BS/MS student at Stanford studying EE.
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Apollo Lee</title>
+      </Head>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-300 to-white">
+        <h1 className="text-5xl font-bold text-gray-900">Apollo Lee</h1>
+        <p className="mt-4 text-lg text-gray-900">
+          I'm a BS/MS student at Stanford studying EE.
+        </p>
+      </div>
+    </>
   );
 }
