68e24f6a243d00599d17cdf1850fcd5ca6358e5e
apollolee3 <apollolee3@gmail.com>
2025-02-12 11:32:04 +0100

    Changed website description

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-300 to-white">
       <h1 className="text-5xl font-bold text-gray-900">Apollo Lee</h1>
       <p className="mt-4 text-lg text-gray-900">
-        I'm a student at Stanford studying EE.
+        I'm a BS/MS student at Stanford studying EE.
       </p>
     </div>
   );
