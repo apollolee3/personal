f4c8ad3af66ae1a475a25722fc5c3fb9a33e5b36
apollolee3 <apollolee3@gmail.com>
2025-02-16 19:05:00 +0100

    Background changes

--- app/page.js
+++ app/page.js
@@ -7,11 +7,11 @@ export default function Home() {
         <title>Apollo Lee</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-300 to-white">
-        <h1 className="text-5xl font-bold text-gray-900 font-[var(--font-cmu-serif)]">
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-5xl font-bold text-black font-[var(--font-cmu-serif)]">
           Apollo Lee
         </h1>
-        <p className="mt-4 text-lg text-gray-900 font-[var(--font-cmu-serif)]">
+        <p className="mt-4 text-xl text-black font-[var(--font-cmu-serif)]">
           I'm a BS/MS student at Stanford studying EE.
         </p>
       </div>
