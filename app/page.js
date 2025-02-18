8790c97a25a7cc0a07b5fa541c2d3db8c73a6ebd
apollolee3 <apollolee3@gmail.com>
2025-02-18 11:41:32 +0100

    Added icons

--- app/page.js
+++ app/page.js
@@ -14,6 +14,22 @@ export default function Home() {
         <p className="mt-4 text-xl text-black font-[var(--font-cmu-serif)]">
           I'm a BS/MS student at Stanford studying EE.
         </p>
+        
+        {/* Social Icons */}
+        <div className="flex space-x-6 mt-6">
+          <a href="https://www.linkedin.com/in/apollo-lee/" target="_blank" rel="noopener noreferrer">
+            <img src="/icons/linkedin-svgrepo-com.svg" alt="LinkedIn" className="w-7 h-7 hover:scale-110 transition-transform" />
+          </a>
+          <a href="https://github.com/apollolee3" target="_blank" rel="noopener noreferrer">
+            <img src="/icons/github-svgrepo-com.svg" alt="GitHub" className="w-7 h-7 hover:scale-110 transition-transform" />
+          </a>
+          <a 
+            href="mailto:apollo1@stanford.edu" 
+            className="relative top-[-3.6px] text-[27px] left-[1.5px] hover:scale-110 transition-transform"
+          >
+            ✉️
+          </a>
+        </div>
       </div>
     </>
   );
